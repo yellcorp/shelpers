bf2d9a3db426d9ed3f9b941960c67f2e539b6077
Jim Boswell <jimb@yellcorp.org>
2018-11-26 16:42:53 -0800

    convert app launchers from aliases to scripts

--- profile.d/stuff.bash
+++ profile.d/stuff.bash
@@ -7,10 +7,4 @@ cdof()
 	cd -- "$(dirname -- "$1")"
 }
 
-alias    gpersp='open -b net.sourceforge.grandperspectiv'
-alias       hex='open -b com.suavetech.0xED'
-alias photoshop='open -b com.adobe.Photoshop'
-alias     stree='open -b com.torusknot.SourceTreeNotMAS'
-alias       vlc='open -b org.videolan.vlc'
-
 alias reveal='open -R'
