c3fa0d433631486bbb356b9483f3b77378661b53
Jim Boswell <jimb@yellcorp.org>
2018-11-30 09:45:28 -0800

    had this thought, jot it down

--- profile.d/stuff.bash
+++ profile.d/stuff.bash
@@ -5,6 +5,9 @@ cdof()
 		echo "Changes to the enclosing directory of FILEPATH" 1>&2
 		return 1
 	fi
+
+	# A simplifcation COULD be to just cd to the specified directory then do
+	# `cd ..`, but this will trash `cd -` to return to the previous directory
 	local container="$(dirname -- "$1")"
 	echo $container
 	cd -- $container
