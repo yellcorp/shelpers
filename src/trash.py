5208a9da4a0b69eadb84bfc962bc63343ed122c4
Jim Boswell <jimb@yellcorp.org>
2024-04-25 22:55:41 -0700

    Increase reliability of trash.py
    Smuggle text out of AppleScript by escaping text. This allows for files
    like Icon<0x13> to be returned safely
    
    Use a proper state machine to parse AppleScript's output

--- src/trash.py
+++ src/trash.py
@@ -1,10 +1,10 @@
+import html
 import os.path
 import subprocess
-from argparse import ArgumentParser
-from collections import deque
-from typing import Deque
-
 import sys
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from typing import Optional
 
 from utils.errors import ErrorReporter
 
@@ -12,26 +12,170 @@ from utils.errors import ErrorReporter
 def get_arg_parser():
     p = ArgumentParser(
         description="""
-                Deletes files or folders by moving them to the Trash.
-            """
+            Deletes files or folders by moving them to the Trash.
+        """
     )
     p.add_argument("files", nargs="+")
     return p
 
 
+# [0] A crude html entity escaping is chosen because AppleScript has no
+#     built-in hex conversion, and HTML entities are an established scheme
+#     that can use base-10 for codepoints.
+#
+# [1] The `id` of a one-character string is a single integer. In all other
+#     cases, it's a list of integers. The id of a zero-character string is
+#     an empty list.
+#
+# [2] Don't know why this is necessary, but if it's not done, integer
+#     equality never returns true. Got the idea from here
+#     https://github.com/mgax/applescript-json/blob/master/json.applescript
+#
+# [3] Characters to escape. Everything below space, plus line delimiters
+#     as understood by `str.splitlines`. Everything else including astral
+#     plane characters should be ok.
+#         32: space
+#         38: ampersand
+#        133: NEL (0x85)
+#       8232: line separator (0x2028)
+#       8233: paragraph separator (0x2029)
+
 SCRIPT = """\
+on escapeString(theString) -- [0]
+    set escaped to ""
+
+    set codepoints to id of theString
+    if (class of codepoints) is not list
+        set codepoints to {codepoints} -- [1]
+    end
+
+    repeat with cc in codepoints
+        set cc to cc as integer -- [2]
+        if cc < 32 or cc = 38 or cc = 133 or cc = 8232 or cc = 8233 then -- [3]
+            set eseq to "&#" & cc & ";"
+        else
+            set eseq to character id cc
+        end
+        set escaped to escaped & eseq
+    end repeat
+    return escaped
+end
+
 on run argv
     repeat with thePath in argv
+        log "fn:" & escapeString(thePath)
         try
             tell application "Finder" to move (thePath as POSIX file) to trash
-            log "ok:" & thePath
-        on error errorText from errorSource
-            log "ef:" & thePath
-            log "et:" & errorText
+            log "ok:1"
+        on error errorText number errorNumber from errorSource
+            log "ok:0"
+            log "et:" & escapeString(errorText as text)
+            log "en:" & escapeString(errorNumber as text)
         end try
     end repeat
 end run
 """
+
+
+@dataclass
+class TrashResult:
+    filename: str = ""
+    ok: bool = False
+    error_text: str = ""
+    error_number: str = ""
+
+
+def split_line(line: str) -> tuple[str, str, str]:
+    return line[:2], line[2:3], line[3:]
+
+
+class LineParser:
+    def __init__(self, error_reporter: Optional[ErrorReporter] = None):
+        self._error_reporter = error_reporter
+
+        self._state = 0
+        self._line_text = ""
+        self._line_n = -1
+        self._panics = 0
+
+        self._result = TrashResult()
+        self._accepted: list[TrashResult] = []
+
+        self._dispatch = [
+            ("fn", self._parse_filename),
+            ("ok", self._parse_ok),
+            ("et", self._parse_error_text),
+            ("en", self._parse_error_number),
+        ]
+
+    @property
+    def protocol_error_count(self):
+        return self._panics
+
+    def parse_lines(self, line_iter):
+        for line_n, line in enumerate(line_iter):
+            self._line_text = line
+            self._line_n = line_n
+
+            prefix, colon, detail_escaped = split_line(line.rstrip("\r\n"))
+            if colon != ":":
+                self._panic("Formatting error")
+                continue
+
+            detail = html.unescape(detail_escaped)
+            expected_prefix, handler = self._dispatch[self._state]
+            if prefix != expected_prefix:
+                self._panic(f"Wrong prefix: {expected_prefix=!r}")
+                continue
+
+            self._state += 1
+            handler(detail)
+
+            if self._accepted:
+                yield from self._accepted
+                self._accepted.clear()
+
+        if self._state != 0:
+            self._panic("Unexpected end of input")
+
+    def _parse_filename(self, detail: str):
+        if detail:
+            self._result.filename = detail
+        else:
+            self._panic("Empty filename")
+
+    def _parse_ok(self, detail: str):
+        if detail == "0":
+            self._result.ok = False
+        elif detail == "1":
+            self._result.ok = True
+            self._accept()
+        else:
+            self._panic("Invalid value for 'ok'")
+
+    def _parse_error_text(self, detail: str):
+        self._result.error_text = detail
+
+    def _parse_error_number(self, detail: str):
+        self._result.error_number = detail
+        self._accept()
+
+    def _accept(self):
+        self._accepted.append(self._result)
+        self._reset()
+
+    def _panic(self, message: str):
+        self._panics += 1
+
+        if self._error_reporter is not None:
+            self._error_reporter.print_error(
+                f"{message} at line {self._line_n} line_text={self._line_text!r}"
+            )
+        self._reset()
+
+    def _reset(self):
+        self._result = TrashResult()
+        self._state = 0
 
 
 def main():
@@ -52,41 +196,35 @@ def main():
     script_proc.stdin.write(SCRIPT)
     script_proc.stdin.close()
 
-    error_files: Deque[str] = deque()
-    error_texts: Deque[str] = deque()
-    error_count = 0
+    parser = LineParser(reporter)
+    consume_errors = 0
+    failures = 0
+    for file_index, result in enumerate(parser.parse_lines(script_proc.stderr)):
+        if file_index < len(args.files):
+            user_file = args.files[file_index]
+            if result.ok:
+                print(user_file)
+            else:
+                message = [
+                    result.error_text or "<no error text>",
+                ]
 
-    def flush_errors():
-        while len(error_files) > 0 and len(error_texts) > 0:
-            error_file = error_files.popleft()
-            error_message = error_texts.popleft()
-            reporter.print_error(error_message, subject_file=error_file)
+                if result.error_number:
+                    message.append(f" ({result.error_number})")
 
-    for protocol_line in script_proc.stderr:
-        if protocol_line.endswith("\n"):
-            protocol_line = protocol_line[:-1]
-        if protocol_line.startswith("ok:"):
-            print(protocol_line[3:])
-        elif protocol_line.startswith("ef:"):
-            error_count += 1
-            error_files.append(protocol_line[3:])
-        elif protocol_line.startswith("et:"):
-            error_texts.append(protocol_line[3:])
+                reporter.print_error("".join(message), subject_file=user_file)
+                failures += 1
         else:
-            print(f"?? {protocol_line!r}", file=sys.stderr)
+            consume_errors += 1
+            reporter.print_error(f"Excess result: {result!r}")
 
-        flush_errors()
-    flush_errors()
+    if consume_errors > 0 or parser.protocol_error_count > 0:
+        return 2
 
-    for prefix, queue in (("ef", error_files), ("et", error_texts)):
-        for superfluous in queue:
-            print(f"?? {prefix} {superfluous!r}", file=sys.stderr)
+    if failures > 0:
+        return 1
 
-    osascript_exit = script_proc.wait(timeout=30)
-    if osascript_exit != 0:
-        return osascript_exit
-
-    return 0 if error_count == 0 else 1
+    return 0
 
 
 if __name__ == "__main__":
